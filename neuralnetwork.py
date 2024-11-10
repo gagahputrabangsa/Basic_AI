34371c728ddc6c970d51c59a810e9afffa480517
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-11 04:10:53 +0700

    Update neuralnetwork.py

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -17,3 +17,9 @@ data = data[data['Species'].isin(['Iris-setosa', 'Iris-versicolor'])]
 
 X = data.iloc[:, :-1].values  # Fitur
 y = (data['Species'] == 'Iris-setosa').astype(int).values  # Label (0 atau 1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
