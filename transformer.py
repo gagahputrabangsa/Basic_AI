62872ad561ee9650f0ec53aa0da7c4b0e65e7bec
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-13 11:27:44 +0700

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -4,3 +4,6 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import cross_val_score
 from nltk.corpus import stopwords
 import nltk
+# Download stopwords
+nltk.download('stopwords')
+stop_words = stopwords.words('english')
