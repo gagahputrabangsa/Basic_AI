9d5201383055f1f1a5abb8cf42d612f87c30ce7a
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-13 11:27:29 +0700

    Create transformer.py

---
+++ transformer.py
@@ -0,0 +1,6 @@
+# Import libraries
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import cross_val_score
+from nltk.corpus import stopwords
+import nltk
