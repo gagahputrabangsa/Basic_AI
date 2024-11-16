fd3630a13129f636df2f3d888b1578a6f9f57a67
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-17 03:29:54 +0700

    load model and token

--- word_translation.py
+++ word_translation.py
@@ -3,3 +3,6 @@ from transformers import MarianMTModel, MarianTokenizer
 def translate_to_english(text, source_lang="id", target_lang="en"):
     # set up marianMT model for translating 
     model_name = f'Helsinki-NLP/opus-mt-{source_lang}-{target_lang}'
+    # Load model and tokenizer
+    tokenizer = MarianTokenizer.from_pretrained(model_name)
+    model = MarianMTModel.from_pretrained(model_name)
