5016f6d1a13a50181c398bb69d5369992e8e90a2
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-17 03:30:36 +0700

    tokenize input

--- word_translation.py
+++ word_translation.py
@@ -6,3 +6,6 @@ def translate_to_english(text, source_lang="id", target_lang="en"):
     # Load model and tokenizer
     tokenizer = MarianTokenizer.from_pretrained(model_name)
     model = MarianMTModel.from_pretrained(model_name)
+
+    # Tokenize input text
+    translated = tokenizer(text, return_tensors="pt", padding=True)
