2c118ed0fe9c5f7e7186868ffff3d11d55036cb2
GagaPutra <91167860+gagahputrabangsa@users.noreply.github.com>
2024-11-18 14:39:13 +0700

    draw a square around the detected faces

--- computervisionpart1.py
+++ computervisionpart1.py
@@ -22,3 +22,14 @@ while True:
 
     # reading the face throughout the webcam
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    
+    # draw a square around the detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    # showing the frame
+    cv2.imshow('Face Detection', frame)
+
+    # shortcut for quit the program (using q)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
